77ef6eb4aca1b120a6dc8eeb38269e7b2fd6a8d6
Otterlord <yourotterlord@gmail.com>
2019-05-31 14:54:57 -0700

    fixes

--- ShufflePuck Cafe Clone/Assets/Scripts/Whacker.cs
+++ ShufflePuck Cafe Clone/Assets/Scripts/Whacker.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Whacker : MonoBehaviour
 {
+    private Rigidbody rb;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector3(-Input.GetAxisRaw("Mouse X") * speed, 0, -Input.GetAxisRaw("Mouse Y") * speed);
     }
 }
